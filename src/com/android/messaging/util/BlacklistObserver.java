LineageOS/android_packages_apps_Messaging

Blocked contact shows in favourite list
Blocked contacts display in favourite list. After fixes,
blocked contacts not dispalying in favourite list

Bug:CYNGNOS-2667
Change-Id: I590c24e162070e1797aaa79bf61c5b98ed9f85a1

src/com/android/messaging/util/BlacklistObserver.java
@@ -64,6 +64,7 @@ public class BlacklistObserver extends ContentObserver {
                     int blockedIndex = cursor.getColumnIndex("message");
                     int nonNormalizedNumberIndex = cursor.getColumnIndex("number");
                     int regexIndex = cursor.getColumnIndex("is_regex");
+                    int favCalBlockedIndex = cursor.getColumnIndex("phone");
                     // if the column indices are not valid, don't perform the queries
                     if (normalizedNumberIndex < 0 || blockedIndex < 0) {
                         if (cursor != null) {
@@ -106,6 +107,17 @@ public class BlacklistObserver extends ContentObserver {
                                         .unarchiveConversation(conversationId);
                             }
                             MessagingContentProvider.notifyParticipantsChanged(conversationId);
+                        }
+                        String favCalBlocked = cursor.getString(favCalBlockedIndex);
+                        boolean isFavCalBlocked = favCalBlocked.compareTo("1") == 0;
+                        String lookupKey = BugleDatabaseOperations
+                                .getLookUpFromOtherParticipantDestination(db, number);
+
+                        if (lookupKey != null) {
+                            Uri lookupUri = Uri.parse(BugleDatabaseOperations.lookUpUri + lookupKey);
+                            if (isFavCalBlocked) {
+                                BugleDatabaseOperations.setStarred(lookupUri, !isFavCalBlocked);
+                            }
                         }
                     }
                 } catch (Exception e) {

src/com/android/messaging/util/BlacklistSync.java
@@ -56,6 +56,7 @@ public class BlacklistSync extends AsyncTask<Void, Void, Void> {
             int blockedIndex = cursor.getColumnIndex("message");
             int nonNormalizedNumberIndex = cursor.getColumnIndex("number");
             int regexIndex = cursor.getColumnIndex("is_regex");
+            int favCalBlockedIndex = cursor.getColumnIndex("phone");
             int updateCount;
             if (normalizedNumberIndex < 0 || blockedIndex < 0) {
                 cursor.close();
@@ -87,6 +88,17 @@ public class BlacklistSync extends AsyncTask<Void, Void, Void> {
                         db.setTransactionSuccessful();
                     } finally {
                         db.endTransaction();
+                    }
+                }
+                String favCalBlocked = cursor.getString(favCalBlockedIndex);
+                boolean isFavCalBlocked = favCalBlocked.compareTo("1") == 0;
+                String lookupKey = BugleDatabaseOperations
+                        .getLookUpFromOtherParticipantDestination(db, number);
+
+                if (lookupKey != null) {
+                    Uri lookupUri = Uri.parse(BugleDatabaseOperations.lookUpUri + lookupKey);
+                    if (isFavCalBlocked) {
+                        BugleDatabaseOperations.setStarred(lookupUri, !isFavCalBlocked);
                     }
                 }
             }

blinga6/cyanide-packages_apps_Messaging

src/com/android/messaging/util/BlacklistObserver.java
@@ -63,7 +63,7 @@ public class BlacklistObserver extends ContentObserver {
                     cursor = mResolver.query(uri, null, null, null, null);
                     int normalizedNumberIndex = cursor.getColumnIndex("normalized_number");
                     int blockedIndex = cursor.getColumnIndex("message");
-
+                    int favCalBlockedIndex = cursor.getColumnIndex("phone");
                     // if the column indices are not valid, don't perform the queries
                     if (normalizedNumberIndex < 0 || blockedIndex < 0) {
                         if (cursor != null) {
@@ -119,7 +119,17 @@ public class BlacklistObserver extends ContentObserver {
                             }
                             MessagingContentProvider.notifyParticipantsChanged(conversationId);
                         }
+                        String favCalBlocked = cursor.getString(favCalBlockedIndex);
+                        boolean isFavCalBlocked = favCalBlocked.compareTo("1") == 0;
+                        String lookupKey = BugleDatabaseOperations
+                                .getLookUpFromOtherParticipantDestination(db, number);
 
+                        if (lookupKey != null) {
+                            Uri lookupUri = Uri.parse(BugleDatabaseOperations.lookUpUri + lookupKey);
+                            if (isFavCalBlocked) {
+                                BugleDatabaseOperations.setUnStarred(lookupUri, !isFavCalBlocked);
+                            }
+                        }
                         if (blockedNumberDeleted) {
                             break;
                         }

src/com/android/messaging/util/BlacklistSync.java
@@ -54,6 +54,7 @@ public class BlacklistSync extends AsyncTask<Void, Void, Void> {
         if (cursor != null && cursor.getCount() > 0) {
             int normalizedNumberIndex = cursor.getColumnIndex("normalized_number");
             int blockedIndex = cursor.getColumnIndex("message");
+            int favCalBlockedIndex = cursor.getColumnIndex("phone");
             int updateCount;
             if (normalizedNumberIndex < 0 || blockedIndex < 0) {
                 cursor.close();
@@ -85,6 +86,17 @@ public class BlacklistSync extends AsyncTask<Void, Void, Void> {
                         db.setTransactionSuccessful();
                     } finally {
                         db.endTransaction();
+                    }
+                }
+                String favCalBlocked = cursor.getString(favCalBlockedIndex);
+                boolean isFavCalBlocked = favCalBlocked.compareTo("1") == 0;
+                String lookupKey = BugleDatabaseOperations
+                        .getLookUpFromOtherParticipantDestination(db, number);
+
+                if (lookupKey != null) {
+                    Uri lookupUri = Uri.parse(BugleDatabaseOperations.lookUpUri + lookupKey);
+                    if (isFavCalBlocked) {
+                        BugleDatabaseOperations.setUnStarred(lookupUri, !isFavCalBlocked);
                     }
                 }
             }